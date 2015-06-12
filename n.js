2ffe73fbd3a627e6c28c74b0869ae972c58af413
pquiring <pquiring@gmail.com>
2015-06-12 09:37:57 -0400

    Create n.js

---
+++ n.js
@@ -0,0 +1,47 @@
+document.body.innerHTML =
+
+"<table><tr>" +
+
+"<td><a href='http://google.ca'><img src='http://google.ca/favicon.ico'>" +
+"<br>Google Search</a></td>" +
+
+"<td><a href='http://gmail.com'><img src='http://icons.iconarchive.com/icons/cornmanthe3rd/plex/512/Communication-gmail-icon.png'>" +
+"<br>Google Mail</a></td>" +
+
+"<td><a href='http://plus.google.com'><img src='http://plus.google.com/favicon.ico'>" +
+"<br>Google Plus</a></td>" +
+
+"<td><a href='http://news.google.com'><img src='https://ssl.gstatic.com/news-static/img/favicon.ico'>" +
+"<br>Google News</a></td>" +
+
+"</tr><tr>" +
+
+"<td><a href='http://drive.google.com'><img src='http://drive.google.com/favicon.ico'>" +
+"<br>Google Drive</a></td>" +
+
+"<td><a href='http://calendar.google.com'><img src='http://calendar.google.com/googlecalendar/images/favicon_v2013_20.ico'>" +
+"<br>Google Calendar</a></td>" +
+
+"<td><a href='http://maps.google.com'><img src='http://maps.gstatic.com/favicon3.ico'>" +
+"<br>Google Maps</a></td>" +
+
+"<td><a href='http://play.google.com'><img src='https://ssl.gstatic.com/android/market_images/web/favicon.ico'>" +
+"<br>Google Play</a></td>" +
+
+"</tr><tr>" +
+
+"<td><a href='http://translate.google.com'><img src='http://translate.google.com/favicon.ico'>" +
+"<br>Google Translate</a></td>" +
+
+"<td><a href='http://youtube.com'><img src='http://youtube.com/favicon.ico'>" +
+"<br>YouTube</a></td>" +
+
+"<td><a href='http://www.facebook.com'><img src='http://www.facebook.com/favicon.ico'>" +
+"<br>Facebook</a></td>" +
+
+"<td><a href='http://www.pandora.com'><img src='http://www.pandora.com/favicon.ico'>" +
+"<br>Pandora</a></td>" +
+
+"</tr></table>"
+
+;

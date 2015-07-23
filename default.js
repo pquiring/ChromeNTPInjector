50946a78fe43054654c8807546dc2d0d3a9f485e
Peter Quiring <pquiring@gmail.com>
2015-07-23 13:08:38 -0400

    icons must be loaded with https (secure)

--- default.js
+++ default.js
@@ -1,16 +1,16 @@
 function defaultHTML() {
   return "<table><tr>" +
 
-  "<td><a href='http://google.ca'><img src='http://google.ca/favicon.ico'>" +
+  "<td><a href='http://google.ca'><img src='https://google.ca/favicon.ico'>" +
   "<br>Google Search</a></td>" +
 
-  "<td><a href='http://gmail.com'><img src='http://icons.iconarchive.com/icons/cornmanthe3rd/plex/512/Communication-gmail-icon.png'>" +
+  "<td><a href='http://mail.google.com'><img src='https://ssl.gstatic.com/ui/v1/icons/mail/images/favicon5.ico'>" +
   "<br>Google Mail</a></td>" +
 
   "<td><a href='http://contacts.google.com'><img src='https://ssl.gstatic.com/s2/oz/images/contacts/favicon-contacts2.ico'>" +
   "<br>Google Contacts</a></td>" +
 
-  "<td><a href='http://plus.google.com'><img src='http://plus.google.com/favicon.ico'>" +
+  "<td><a href='http://plus.google.com'><img src='https://plus.google.com/favicon.ico'>" +
   "<br>Google Plus</a></td>" +
 
   "<td><a href='http://news.google.com'><img src='https://ssl.gstatic.com/news-static/img/favicon.ico'>" +
@@ -18,30 +18,30 @@ function defaultHTML() {
 
   "</tr><tr>" +
 
-  "<td><a href='http://drive.google.com'><img src='http://drive.google.com/favicon.ico'>" +
+  "<td><a href='http://drive.google.com'><img src='https://drive.google.com/favicon.ico'>" +
   "<br>Google Drive</a></td>" +
 
-  "<td><a href='http://calendar.google.com'><img src='http://calendar.google.com/googlecalendar/images/favicon_v2013_20.ico'>" +
+  "<td><a href='http://calendar.google.com'><img src='https://calendar.google.com/googlecalendar/images/favicon_v2013_20.ico'>" +
   "<br>Google Calendar</a></td>" +
 
-  "<td><a href='http://maps.google.com'><img src='http://maps.gstatic.com/favicon3.ico'>" +
+  "<td><a href='http://maps.google.com'><img src='https://maps.gstatic.com/favicon3.ico'>" +
   "<br>Google Maps</a></td>" +
 
   "<td><a href='http://play.google.com'><img src='https://ssl.gstatic.com/android/market_images/web/favicon.ico'>" +
   "<br>Google Play</a></td>" +
 
-  "<td><a href='http://translate.google.com'><img src='http://translate.google.com/favicon.ico'>" +
+  "<td><a href='http://translate.google.com'><img src='https://translate.google.com/favicon.ico'>" +
   "<br>Google Translate</a></td>" +
 
   "</tr><tr>" +
 
-  "<td><a href='http://youtube.com'><img src='http://youtube.com/favicon.ico'>" +
+  "<td><a href='http://youtube.com'><img src='https://youtube.com/favicon.ico'>" +
   "<br>YouTube</a></td>" +
 
-  "<td><a href='http://www.facebook.com'><img src='http://www.facebook.com/favicon.ico'>" +
+  "<td><a href='http://www.facebook.com'><img src='https://www.facebook.com/favicon.ico'>" +
   "<br>Facebook</a></td>" +
 
-  "<td><a href='http://www.pandora.com'><img src='http://www.pandora.com/favicon.ico'>" +
+  "<td><a href='http://www.pandora.com'><img src='https://www.pandora.com/favicon.ico'>" +
   "<br>Pandora</a></td>" +
 
   "</tr></table>"
